a1ff1c94f31546070d4082080c118337372145f7
TiFAu <turchin.anton2@gmail.com>
2021-02-13 00:07:05 +0200

    task-10 Object.create happy end

--- task-10 exampl us Object.create-14.js
+++ task-10 exampl us Object.create-14.js
@@ -1,1 +1,16 @@
-var obj = O
+var obj = Object.create(
+    (
+        function(){
+            return Object.defineProperty({}, "keys", {
+                get(){
+                    return Object.keys(this)
+                },
+                set (prop) {
+                    this [prop[0]] = prop[1]
+                }
+            })
+        }
+    )()
+)
+obj.keys = ["service","google"]
+console.log (obj.keys[0])
